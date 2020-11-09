233f5cf4f43cea9de7273d76f634eec4f3daca82
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-09 11:16:17 +0100

    dank

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose');
+let http = require('http');
 
 mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
     { useNewUrlParser: true, useUnifiedTopology: true });
+
+    // process.env.PORT lets the port be set by heruko
+    let port = process.env.PORT || 8080;
+    
+    http.createServer(function(req, res){
+        res.writeHead(200);
+        res.write("Amanda Leighton Schjoedt er en cutie\n");
+        res.write("Amanda Leighton Schjoedt er en cutie\n");
+        res.end();
+    }).listen(port);
+    
+    console.log("Listening on the port " + port + "...");
