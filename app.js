1975531c6840ab7a957f1ef6193d466b73eb88f8
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-09 12:40:17 +0100

    testerbester

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 let http = require('http');
+let express = require('express');
 
 mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
     { useNewUrlParser: true, useUnifiedTopology: true });
