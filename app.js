e774bba42c2c6661810fdf1de857d13ddbea8f98
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-09 13:01:41 +0100

    sdf

--- app.js
+++ app.js
@@ -4,20 +4,16 @@ let express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 
-mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
-    { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
+//     { useNewUrlParser: true, useUnifiedTopology: true });
 
     // process.env.PORT lets the port be set by heruko
     let port = process.env.PORT || 8080;
-    
-    http.createServer(function(req, res){
-        res.writeHead(200);
-        res.write('victor har en micro penis');
-        res.end();
-    }).listen(port);
-
+    app.get('/', function(req, res){
+        res.send('weenis')
+    })
     app.get('/test', function(req, res) {
-        res.write('Penis');
+        res.send('nenis');
     });
-    
-    console.log("Listening on the port " + port + "...");
+    app.listen(port, ()=>{  console.log("Listening on the port " + port + "...");})
+ 
