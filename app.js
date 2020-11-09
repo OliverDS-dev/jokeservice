b9f361dc0adafc80a2810ea07ef48ac9b11af673
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-09 12:49:47 +0100

    pushoit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 let http = require('http');
 let express = require('express');
+const app = express();
+app.set('view engine', 'pug');
 
 mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
     { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,13 +12,12 @@ mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_re
     
     http.createServer(function(req, res){
         res.writeHead(200);
-        res.write("Amanda Leighton Schjoedt er en cutie\n");
-        res.write("Amanda Leighton Schjoedt er en cutie\n");
+        res.write('victor har en micro penis');
         res.end();
     }).listen(port);
 
-    http.get('/test', function(req, res) {
-        res.send('Penis');
+    app.get('/test', function(req, res) {
+        res.write('Penis');
     });
     
     console.log("Listening on the port " + port + "...");
