0b3e2cfa0ef30f4fc946810d7618dc97d5fb5564
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-11 11:09:55 +0100

    commitu

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let http = require('http');
 let express = require('express');
 const app = express();
 app.set('view engine', 'pug');
-const config = require('./config');
 
 // mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
 //     { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,7 +18,6 @@ const config = require('./config');
     });
 
     //Routes
-    const jokeRouter = require('./routes/jokes');
-
-    app.listen(config.PORT, ()=>{  console.log("Listening on the port " + port + "...");})
+    // const jokeRouter = require('./routes/jokes');
+    app.listen(port, ()=>{  console.log("Listening on the port " + port + "...");});
  
