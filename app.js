8a11e5964e247ce00d3e493c0bf137a14f2be8c0
Victor Nielsen <vnillers@gmail.com>
2020-11-13 14:21:21 +0100

    Fixed lmao

--- app.js
+++ app.js
@@ -25,34 +25,6 @@ mongoose.connect('mongodb+srv://joker:cqwNfOUXXNADTpRr@erebus.84dlo.mongodb.net/
     const fetch = require('node-fetch');
 let registryUrl = 'https://krdo-joke-registry.herokuapp.com/api/services';
 
-async function post(url, objekt) {
-    const respons = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(objekt),
-        headers: { 'Content-Type': 'application/json' }
-    });
-    if (respons.status !== 200) // OK
-    // if (respons.status !== 201) // Created
-        throw new Error(respons.status);
-    return await respons.json();
-}
-
-async function main(url) {
-    try {
-        let respons = await post(url,
-            {
-                "name": "DAVO Jokeservice",
-                "address": "https://jokeservicedao.herokuapp.com/",
-                "secret": "i like men",
-            }
-        );
-        console.log(respons);
-    } catch (fejl) {
-        console.log(fejl);
-    }
-    process.exit();
-}
-main(registryUrl);
 
     app.listen(port, ()=>{  console.log("Listening on the port " + port + "...");});
  
