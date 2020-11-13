44f986884ac13ba78769a42e65d62209c0b84e38
OliverDyhrSpatzek <oliverspatzek@gmail.com>
2020-11-13 13:54:22 +0100

    aaalllmost final

--- app.js
+++ app.js
@@ -10,33 +10,17 @@ app.set('view engine', 'pug');
 mongoose.connect('mongodb+srv://joker:cqwNfOUXXNADTpRr@erebus.84dlo.mongodb.net/Jokes?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true
   });
-// mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
-//     { useNewUrlParser: true, useUnifiedTopology: true });
-
     // process.env.PORT lets the port be set by heruko
     let port = process.env.PORT || 8080;
     app.get('/', function(req, res){
         res.render('frontpage');
     })
-    
-    // app.get('/jokes', function(req, res) {
-    //     res.render('jokes');
-    // });
-    // app.get('/api/otherjokes', function(req, res) {
-    //     res.render('otherjokes');
-    // });
-
-    // app.get('/api/othersites', function(req, res){
-    //     res.render('othersites');
-    // });
 
     //Routes
     const jokeRouter = require('./routes/jokes');
     const otherSiteRouter = require('./routes/otherSites');
-    const otherJokesRouter = require('./routes/otherJokes')
     app.use(jokeRouter);
     app.use(otherSiteRouter);
-    app.use(otherJokesRouter);
 
     app.listen(port, ()=>{  console.log("Listening on the port " + port + "...");});
  
